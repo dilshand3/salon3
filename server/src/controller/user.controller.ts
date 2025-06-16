c84b8a8aa7f592933a65f3d1103b9c836d7e711c
dilshan.d3 <dilshan72404@gmail.com>
2025-06-16 23:13:35 +0530

    Add bcryptjs for password hashing and update completeProfile to include email validation

--- server/src/controller/user.controller.ts
+++ server/src/controller/user.controller.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import { User } from "../model/user.model";
+import bcrypt from "bcryptjs";
 
 interface IsignupRes {
     success: boolean;
@@ -41,6 +42,7 @@ export const signUp = async (req: Request, res: Response<IsignupRes>): Promise<a
 }
 
 interface IcompleteProfileReq {
+    email: string;
     name: string;
     password: string;
     number: string;
@@ -54,14 +56,39 @@ interface IcompleteProfileRes {
 
 export const completeProfile = async (req: Request<{}, {}, IcompleteProfileReq>, res: Response<IcompleteProfileRes>): Promise<any> => {
     try {
-        const { name, password, number } = req.body;
-        if (!name || !password || !number) {
-           return res.status(400).json({
-            success : false,
-            message : "All field required!"
-           })
+        const { email, name, password, number } = req.body;
+        if (!email || !name || !password || !number) {
+            return res.status(400).json({
+                success: false,
+                message: "All field required!"
+            })
         }
 
+        const hashedpassword = await bcrypt.hash(password,10)
+
+        const updatedUser = await User.findByIdAndUpdate(
+            { email },
+            {
+                name,
+                password : hashedpassword,
+                number
+            }
+        )
+
+        if (!updatedUser) {
+            return res.status(400).json({
+                success: false,
+                message: "User not found"
+            })
+        }
+
+        res.status(200).json({ 
+            success: true,
+            message: "Profile updated successfully",
+            data: {
+                updatedUser
+            }
+        })
     } catch (error) {
         console.log(error);
         res.status(500).json({
